11895db29d39a5f11812068012c683c08a63f6ab
sheepcao <sheepcao1986@163.com>
2013-10-19 10:34:56 +0800

    Initial Commit

---
+++ DaysInLine/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DaysInLine
+//
+//  Created by 张力 on 13-10-19.
+//  Copyright (c) 2013年 张力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DaysInLine/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DaysInLine
+//
+//  Created by 张力 on 13-10-19.
+//  Copyright (c) 2013年 张力. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
